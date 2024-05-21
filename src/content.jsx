2b977a810840403c92b72b5f8c9fa3f571d580fe
Nishanth Duraisamy <nishanthduraisamy@gmail.com>
2024-05-22 00:06:47 +0530

    password-localstorage-added

--- src/content.jsx
+++ src/content.jsx
@@ -9,10 +9,20 @@ import {
   Checkbox,
   FormHelperText,
 } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { generateRandomPassword } from './utils/generals.js';
-import { ContentCopy, CheckCircle } from '@mui/icons-material';
-// import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { CheckCircle } from '@mui/icons-material';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Tooltip,
+} from '@mui/material';
+import { FileCopy } from '@mui/icons-material';
 
 function Content(props) {
   const [password, setPassword] = useState('');
@@ -23,6 +33,18 @@ function Content(props) {
     includeAlphabets: true,
   });
   const [checkErr, setCheckErr] = useState(false);
+  const [lastFivePass, setLastFivePass] = useState([]);
+  const [lastFivePassCopy, setLastFivePassCopy] = useState(-1);
+
+  // load last five passwords form local
+  useEffect(() => {
+    let passwords = localStorage.getItem('passwords');
+    if (passwords) {
+      passwords = JSON.parse(passwords);
+      setLastFivePass(passwords); // show last 5 passwords
+      setPassword(passwords[0].password); // show last password in ui
+    }
+  }, []);
 
   const generatePassword = () => {
     if (
@@ -35,6 +57,16 @@ function Content(props) {
     }
     const password = generateRandomPassword(10, passwordOptions);
     setPassword(password);
+
+    // persist the password into local storage
+    let passwords = [...lastFivePass];
+    passwords.unshift({
+      password,
+      time: Date.now(),
+    });
+    passwords = passwords.slice(0, 5); // store only last 5 password
+    setLastFivePass(passwords);
+    localStorage.setItem('passwords', JSON.stringify(passwords));
   };
 
   const handleCopyPassword = () => {
@@ -97,6 +129,20 @@ function Content(props) {
     },
   ];
 
+  const handleTableCopy = (text, index) => {
+    navigator.clipboard.writeText(text).then(() => {
+      setLastFivePassCopy(index);
+      setTimeout(() => {
+        setLastFivePassCopy(-1);
+      }, 2000); // Reset the copy success state after 2 seconds
+    });
+  };
+
+  const getDate = (date) => {
+    const str = new Date(date);
+    return `${str.toDateString()}, ${str.toLocaleTimeString()}`;
+  };
+
   return (
     <Box display='flex' flexDirection='column' gap={2} alignItems='center'>
       {/* show the generated password */}
@@ -111,13 +157,15 @@ function Content(props) {
         InputProps={{
           endAdornment: (
             <InputAdornment position='end'>
-              <IconButton
-                aria-label='toggle password visibility'
-                onClick={handleCopyPassword}
-                edge='end'
-              >
-                {passwordCopied ? <CheckCircle /> : <ContentCopy />}
-              </IconButton>
+              <Tooltip title={passwordCopied ? 'Copied!' : 'Copy to clipboard'}>
+                <IconButton
+                  aria-label='toggle password visibility'
+                  onClick={handleCopyPassword}
+                  edge='end'
+                >
+                  {passwordCopied ? <CheckCircle /> : <FileCopy />}
+                </IconButton>
+              </Tooltip>
             </InputAdornment>
           ),
         }}
@@ -155,6 +203,48 @@ function Content(props) {
       >
         Click To Generate New Password
       </Button>
+
+      {/* show last five password */}
+      {lastFivePass.length > 0 && (
+        <TableContainer component={Paper} sx={{ mt: 4 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Generation time</TableCell>
+                <TableCell>Password</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {lastFivePass.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>{getDate(row.time)}</TableCell>
+                  <TableCell>{row.password}</TableCell>
+                  <TableCell>
+                    <Tooltip
+                      title={
+                        lastFivePassCopy === index
+                          ? 'Copied!'
+                          : 'Copy to clipboard'
+                      }
+                    >
+                      <IconButton
+                        onClick={() => handleTableCopy(row.password, index)}
+                      >
+                        {lastFivePassCopy === index ? (
+                          <CheckCircle />
+                        ) : (
+                          <FileCopy />
+                        )}
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </Box>
   );
 }
